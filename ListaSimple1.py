24ae27b73e44edfad9b7bffc5406fad09a1b3315
joseemelendez <2533932800101@ingenieria.usac.edu.gt>
2022-01-23 00:31:16 -0600

    Agregado eliminacion

--- ListaSimple1.py
+++ ListaSimple1.py
@@ -54,6 +54,80 @@ class Lista:
                             print("No se ha encontrado el estudiante.", "\n")
                         else :
                             nodo = nodo.siguiente
+
+        def eliminar (self, carnet):
+            
+            nodo = self.cabeza
+
+            if nodo is None: #la lista está vacia
+                print("No se puede eliminar, lista vacía.")
+            elif nodo.siguiente is None: #tiene solo un elemento
+                if nodo.carnet == carnet:
+                    print("Se ha encontrado el carnet: ", nodo.carnet)
+                    self.tamanio -= 1
+                    self.cabeza = None
+                else:
+                    print("No se ha encontrado el elemento.")
+            else: #tiene mas de un elemento
+                if nodo.carnet == carnet:#es el primer elemento
+                    print("Se ha encontrado el carnet: ", nodo.carnet)
+                    self.tamanio -= 1
+                    self.cabeza = nodo.siguiente
+                else:#no es el primero
+                    print("El elemento a borrar no es el primero")
+                    anterior = nodo
+                    nodo = anterior.siguiente
+                    while nodo is not None:
+                        if nodo.carnet == carnet:
+                            anterior.siguiente = nodo.siguiente
+                            self.tamanio -=1
+                            break
+                        else:
+                            anterior = nodo
+                            nodo = anterior.siguiente
+                    
+                    if nodo is None:
+                        print("No se encontró el carnet a eliminar.")
+                            
+
+
+                   
+                        
+
+
+
+
+
+                #if nodo is None: #tiene solo un elemento
+                
+                #else:
+
+
+
+
+            # if self.cabeza is None: #si está vacia
+            #     print("No se puede eliminar, lista vacia.")
+            # else:
+            #     nodo = self.cabeza
+            #     seguir = True
+            #     if nodo.carnet == carnet: #si tiene solo un elemento
+            #         self.cabeza = None
+            #         self.tamanio -= 1
+            #         print("Se eliminado el alumno: ", nodo.carnet)
+            #     else: #si tiene mas elementos
+            #         while seguir:
+                        
+            #             if nodo.carnet == carnet:
+            #                 anterior = nodo
+            #                 anterior.siguiente = nodo.siguiente
+            #                 self.tamanio -=1
+            #                 seguir = False
+            #                 print("Se eliminado el alumno: ", carnet)
+                            
+            #             else:
+            #                 nodo = nodo.siguiente
+
+            pass
             
         def visualizar(self):
             actual = self.cabeza
@@ -68,9 +142,9 @@ class Lista:
         
 lista = Lista()
 
-lista.insertarInicio(1, "Jose")
-lista.insertarInicio(2, "Carlos")
-lista.insertarInicio(3, "Lucia")
+#lista.insertarInicio(1, "Jose")
+#lista.insertarInicio(2, "Carlos")
+#lista.insertarInicio(3, "Lucia")
 lista.insertarInicio(4, "Juana")
 
 lista.insertarFinal(5, "Carmen")
@@ -80,6 +154,11 @@ lista.insertarInicio(7, "Pablo")
 lista.insertarFinal(8, "Carolina")
 
 lista.visualizar()
+lista.eliminar(10)
+lista.visualizar()
 
 
-lista.buscar(8)
+#lista.buscar(8)
+
+#lista.visualizar()
+#lista.visualizar()
